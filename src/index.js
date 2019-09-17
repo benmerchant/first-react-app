07c06c8ee4b5ea01f125bc2da247d5fdc04a1d35
benmerchant <benjaminmerchant@gmail.com>
2019-09-17 01:13:22 -0400

    Lifting State Up

--- src/index.js
+++ src/index.js
@@ -3,23 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {value:null,};
-  }
+  /** 
+   * no need for a constructor as this class does not maintain state
+   * it merely displays what the parent tells it to
+  */
   render() {
     return (
-      <button className="square" onClick={()=>this.setState({value:'X'})}>
-        {this.state.value}
+      /** 
+       * 1. onClick() tells React to setup an event listener
+       * 2. when btn clicked, React calls the event handler
+       * 3. event handler calls function which is a prop specified by Board
+       * 4. Square calls this.handleClick(i) when clicked
+       * 5. we havent defined that ^^^^ function yet
+      */
+      <button className="square" onClick={()=>this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
   
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i}/>;
+  /**
+   * add constructor to initial state to contain an array of 9 nulls
+   * corresponding to the 9 squares
+   */
+  constructor(props){
+    super(props);
+    this.state = {squares: Array(9).fill(null),};
   }
+  handleClick(i){
+    // creating a copy of squares[] to modify... why?
+    const squares = this.state.squares.slice();
+    squares[i] = '╗';
+    this.setState({squares: squares});
+  }
+  /** 
+   * instruct each Square about its value by passing props
+   * the square are defined in the immediately preceding constructor
+   */
+  renderSquare(i) {
+    return (<Square
+              value={this.state.squares[i]} onClick={()=>this.handleClick(i)}
+            />); // add () so JS won't insert a ';' after 'return'
+  } // now each square will receive a value prop of either 'X','O',or null
 
   render() {
     const status = 'Next player: X';
